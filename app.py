f9c8eb5b403f870e8070951c77716f0273fda52b
Zi0MIX <80110206+Zi0MIX@users.noreply.github.com>
2022-03-06 07:23:03 +0100

    1.2.0
    - Config support for current settings
    - Custom LogFile path added via config support

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os, sys, getpass, re, subprocess
-from time import ctime, time, sleep, localtime, mktime
+from time import ctime, time, sleep
 from pythonping import ping
 
 address_list = []
@@ -10,7 +10,10 @@ reg_reply = re.compile("Reply from")
 
 definitions = {
     "ping_freq": range(1, 301),
-    "ping_threshold": range(1, 2000) # Exclude 2000 as that's the timeout code
+    "ping_threshold": range(1, 2000), # Exclude 2000 as that's the timeout code
+    "cfg_len": 4,
+    "forbidden_symbols": ["*", "?", "\"", "<", ">", "|"],
+    "drive_letters": ["C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 }
 
 defaults = {
@@ -31,9 +34,107 @@ errors = {
     "ping": "Something went wrong, not all pings were sent.",
     "analytics": "An error occured while analyzing the result.",
     "argument": "Provided argument is wrong, try again!",
+    "cfg_logfile": "Wrong path to the log file, applying default.",
 }
 
 ##### GENERAL FUNCTIONS #####
+
+class Config:
+    def __init__(self):
+        self.path = os.path.dirname(__file__)
+
+
+    def open_cfg(self):
+        """Function will check if config file exists in current program directory, if it does then it'll pull data from ot, otherwise it'll trigger building a new config file."""
+        while True:
+            try:
+                configuration_file = open(f"{self.path}\\config.cfg" , "r")
+                data = configuration_file.read()
+                break
+            except FileNotFoundError:
+                Config.build_cfg()
+            finally:
+                configuration_file.close()
+        return data
+
+    
+    def build_cfg(self):
+        """Function serves as a slave to open_cfg, it'll create a config file,"""
+        t_bulid = open(f"{self.path}\\config.cfg", "x")
+        print(f"Config file created in {self.path}")
+        t_bulid.close()
+        return
+
+
+    def verify_cfg(self):
+        """Function will analyze config file and if it finds that the amount of lines doesn't match, it'll force default settings in the config. If it finds issues with logfile path, it'll attempt to fix it."""
+        read_file = Config().read_cfg(1, return_all=True)
+        with open(f"{self.path}\\config.cfg" , "w") as f:
+            # print(f"len of cfg {len(Config().read_cfg(1, return_all=True))}")
+            if len(read_file) != definitions["cfg_len"]:
+                print("Wrong lenght of the config. Generating a new one.")
+                f.write("")
+                t_addresses = ""
+                for x in defaults["address_list"]:
+                    t_addresses += f"{x} "
+                t_addresses = t_addresses[:-1]
+                t_clean_cfg = f"""logfile_path = "{defaults["win_logfile_path"]}"
+addresses = "{t_addresses}"
+sleep = "{defaults["sleep"]}"
+ping_threshold = "{defaults["ping_threshold"]}"
+"""
+                f.write(t_clean_cfg)
+                f.close()
+                return
+                
+            else:
+                logfile_error = False
+                t_syspath = cleanup(read_file[0], "syspath")
+                if t_syspath != read_file[0]:
+                    logfile_error = True
+
+                check_for_drive_letter = t_syspath.split(":")
+                if len(check_for_drive_letter) not in [1, 2]:
+                    logfile_error = True
+                if check_for_drive_letter[0] not in definitions["drive_letters"]:
+                    logfile_error = True
+
+                if logfile_error:
+                    print("Error with logfile path detected. Attempting to fix.")
+                    t_syspath = "C:" + check_for_drive_letter[1]
+
+                t_overwrite_cfg = f"""logfile_path = "{t_syspath}"
+addresses = "{read_file[1]}"
+sleep = "{read_file[2]}"
+ping_threshold = "{read_file[3]}"
+"""
+                f.write(t_overwrite_cfg)
+                f.close()
+                return
+
+
+    def read_cfg(self, read_line, return_all=False):
+        """Function will read data from config file."""
+        t_properties = []
+        with open(f"{self.path}\\config.cfg" , "r", encoding="utf-8") as f:
+            t_cfg = f.read()
+            t_properties = read_arguments(t_cfg)
+        if return_all:
+            return t_properties
+        return str(t_properties[read_line - 1])
+
+
+def read_arguments(input, split1="\n", split2="\""):
+    """Function serves as a slave to read_cfg, it'll pull arguments from config lines."""
+    t_list = []
+    t_fulldata = str(input).split(split1)
+    for x in t_fulldata:
+        t_value = x.split(split2)
+        if len(t_value) != 3:
+            continue
+        t_list.append(t_value[1])
+    return t_list
+
 
 def ask(t_question, t_error, t_lower_it=True, t_definition=["y", "n", None], t_output=None):
     """Function will ask user for the input(str), then validate it against provided list of definitions and return True/False/Output depending the selection."""
@@ -60,15 +161,19 @@ def ask(t_question, t_error, t_lower_it=True, t_definition=["y", "n", None], t_o
             sys.exit()
 
 
-def open_log_file(default_path):
-    """Function will open a log file from provided system path."""
-    try:
-        t_log_file = open(rf"{default_path}\event log.txt", "a")
-    except FileNotFoundError:
-        os.mkdir(default_path)
-        t_log_file = open(rf"{default_path}\event log.txt", "x")
-        print(f"Log file created in {default_path}")
-    return t_log_file
+def write_to_log(default_path, data):
+    """Function will check if log file exists in the given path. If it does, it'll log given data to it, if it doesn't it'll create the file first."""
+    while True:
+        try:
+            with open(f"{default_path}\\event log.txt", "a", encoding="utf-8") as f:
+                f.write(data)
+            break
+        except FileNotFoundError:
+            os.mkdir(default_path)
+            create_file = open(f"{default_path}\\event log.txt", "x")
+            print(f"Log file created in {default_path}")
+            create_file.close()
+    return
 
 
 def analyze_response(response, ip_in, firewall, reg_args):
@@ -99,14 +204,15 @@ def analyze_response(response, ip_in, firewall, reg_args):
 
 def cleanup(t_input, case, argument=2):
     """Function will use regex to modify provided strings so they can be processed further."""
-    t_reg = re.split(" ", t_input)
+    if case != "syspath":
+        t_reg = re.split(" ", t_input)
+
     if case == "ip":
         t_ip = t_reg[argument]
         if argument == 2:
             t_ip = t_ip.replace(",", "")
         return t_ip
     elif case == "time":
-        # print(t_reg)
         pos = 6
         if argument == 1:
             pos = 10
@@ -117,6 +223,12 @@ def cleanup(t_input, case, argument=2):
         elif argument == 2:
             t_time = t_time.replace("Round", "")
         return t_time
+    elif case == "syspath":
+        t_reg = str(t_input)
+        t_reg = t_reg.replace("/", "\\")
+        for x in definitions["forbidden_symbols"]:
+            t_reg = t_reg.replace(x, "")
+        return t_reg           
 
 
 def add_one(t_number, last_time_timeout):
@@ -140,82 +252,118 @@ def checkup(t_time):
 ##### CONFIGURATION #####
 
 while True:
-    while True:
-        use_default_address = ask("Would you like to use default addresses? (Y/N/LIST) ", errors['input'], t_definition=["y", "n", "list"],t_output="8.8.8.8 | 1.1.1.1 | 208.67.222.222 | 9.9.9.9")
-        if use_default_address not in [True, False]:
-            print(use_default_address)
-            continue
-        break
+    use_config = ask("Would you like to use config file? (Y/N) ", errors['input'])
+    if use_config:
+        Config().verify_cfg()
 
-    first_address = True
-    if use_default_address: 
-        max_list_len = 6
-    else: 
-        max_list_len = 10
-
-    while True:
-        while True:
-            if not use_default_address and first_address:
-                skip_question = True
-            else:
-                skip_question = False
-
-            if first_address and not use_default_address:
-                output = "Would you like to add your own, custom address? (Y/N) "
-            else:
-                output = "Would you like to add another address? (Y/N) "
-
-            if first_address and skip_question:
-                append_custom_address = True
-                break
-            else:
-                append_custom_address = ask(output, errors['y/n input'])
-                break
-
-        if not append_custom_address:
+    # Address list
+    if use_config:
+        all_addresses = Config().read_cfg(2)
+        list_of_addresses = str(all_addresses).split(" ")
+        amount_of_addresses_from_cfg = len(list_of_addresses)
+        if amount_of_addresses_from_cfg > 10:
+            input(errors["max_addresses"])
+            sys.exit()
+        for x in list_of_addresses:
+            custom_address_list.append(x)
+        use_default_address = False
+    else:            
+        while True: 
+            use_default_address = ask("Would you like to use default addresses? (Y/N/LIST) ", errors['input'], t_definition=["y", "n", "list"],t_output="8.8.8.8 | 1.1.1.1 | 208.67.222.222 | 9.9.9.9")
+            if use_default_address not in [True, False]:
+                print(use_default_address)
+                continue
             break
-        else:
-            first_address = False
-            address = input("Type address of the host you want to ping ")
-            custom_address_list.append(address)
-            if len(custom_address_list) >= max_list_len:
-                print(errors['max_addresses'])
-                break
 
-    use_default_sleep = ask(f"Would you like to use default time between pings? Default is {defaults['sleep']} (Y/N) ", errors['y/n input'])
-    if not use_default_sleep:
+        first_address = True
+        if use_default_address: 
+            max_list_len = 6
+        else: 
+            max_list_len = 10
+
         while True:
-            print("Provide time between pings in seconds (range 1 to 300)")
-            ping_freq = int(input("> "))
-            if ping_freq in definitions['ping_freq']:
+            while True:
+                if not use_default_address and first_address:
+                    skip_question = True
+                else:
+                    skip_question = False
+
+                if first_address and not use_default_address:
+                    output = "Would you like to add your own, custom address? (Y/N) "
+                else:
+                    output = "Would you like to add another address? (Y/N) "
+
+                if first_address and skip_question:
+                    append_custom_address = True
+                    break
+                else:
+                    append_custom_address = ask(output, errors['y/n input'])
+                    break
+
+            if not append_custom_address:
                 break
             else:
-                print(errors['argument'])
-    else:
-        ping_freq = defaults['sleep']
+                first_address = False
+                address = input("Type address of the host you want to ping ")
+                custom_address_list.append(address)
+                if len(custom_address_list) >= max_list_len:
+                    print(errors['max_addresses'])
+                    break
 
-    use_default_logfile = True #a_question("Would you like to use default logfile location? (Y/N) ", errors["y/n input"], "y/n")
-    if not use_default_logfile:
-        pass # Maybe one day
+    # Sleep
+    if use_config:
+        try:
+            ping_freq = Config().read_cfg(3)
+            ping_freq = int(ping_freq)
+        except ValueError:
+            ping_freq = defaults['sleep']
+            print("Inproper value in the config. Applying default 'sleep' value.")
+    else:
+        use_default_sleep = ask(f"Would you like to use default time between pings? Default is {defaults['sleep']} (Y/N) ", errors['y/n input'])
+        if not use_default_sleep:
+            while True:
+                print("Provide time between pings in seconds (range 1 to 300)")
+                ping_freq = int(input("> "))
+                if ping_freq in definitions['ping_freq']:
+                    break
+                else:
+                    print(errors['argument'])
+        else:
+            ping_freq = defaults['sleep']
+
+    # Logfile
+    if use_config:
+        logfile_error = False
+        logfile_path = Config().read_cfg(1)
+        logfile_path = cleanup(logfile_path, "syspath")
+
     else:
         logfile_path = defaults['win_logfile_path']
     
-    use_default_threshold = ask(f"Would you like to use default limit, above which pings get logged? Default is {defaults['ping_threshold']} (Y/N) ", errors['y/n input'])
-    if not use_default_threshold:
-        while True:
-            print("Type in new ping limit (between 1 and 1999).")
-            try:
-                ping_threshold = float(input("> "))
-            except ValueError:
-                print(errors['int'])
-                continue
-
-            if ping_threshold in definitions['ping_threshold']:
-                break
-            else:
-                print(errors['argument'])
+    # Threshold
+    if use_config:
+        try:
+            ping_threshold = float(Config().read_cfg(4))
+        except ValueError:
+            ping_threshold = defaults['ping_threshold']
+            print("Inproper value in the config. Applying default 'ping_threshold' value.")
     else:
-        ping_threshold = defaults['ping_threshold']
+        use_default_threshold = ask(f"Would you like to use default limit, above which pings get logged? Default is {defaults['ping_threshold']} (Y/N) ", errors['y/n input'])
+        if not use_default_threshold:
+            while True:
+                print("Type in new ping limit (between 1 and 1999).")
+                try:
+                    ping_threshold = float(input("> "))
+                except ValueError:
+                    print(errors['int'])
+                    continue
+
+                if ping_threshold in definitions['ping_threshold']:
+                    break
+                else:
+                    print(errors['argument'])
+        else:
+            ping_threshold = defaults['ping_threshold']
             
     if use_default_address:
         for x in defaults['address_list']:
@@ -233,12 +381,15 @@ while True:
     for x in address_list:
         print(x)
 
-    is_configuration_successful = ask("Would you like to proceed or would you like to configure again? (Y to continue, N to config) ", errors['y/n input'])
-    if is_configuration_successful:
-        break
+    if not use_config:
+        is_configuration_successful = ask("Would you like to proceed or would you like to configure again? (Y to continue, N to config) ", errors['y/n input'])
+        if is_configuration_successful:
+            break
+        else:
+            address_list = []
+            custom_address_list = []
     else:
-        address_list = []
-        custom_address_list = []
+        break
 
 ##### PROCESSOR #####
 time_outs = 0
@@ -292,9 +443,8 @@ else:                           # If it's determined firewall might be blocking 
         print("Do note, alternative method may become unstable over long period of time, please monitor the program.")
         firewall_active = True
 
-logfile = open_log_file(logfile_path)
 print("Pinging ...")
-print(f"Results will be saved in a log in {defaults['win_logfile_path']}\\event log.txt")
+print(f"Results will be saved in a log in {logfile_path}\\event log.txt")
 while True:
     ip_id = 0
     network_change = False
@@ -322,29 +472,34 @@ while True:
         if analyzed[1] >= ping_threshold:   # Print and log high ping / timeout events
             if analyzed[1] < 2000:
                 print(f"Ping higher than {ping_threshold} to {analyzed[0]} at {ctime()} is {analyzed[1]}ms.")
-                logfile.write(f"""{ctime()} Ping to {analyzed[0]} is {analyzed[1]}ms. """)
+                data = (f"""{ctime()} Ping to {analyzed[0]} is {analyzed[1]}ms.\n""")
+                write_to_log(logfile_path, data)
                 if analyzed[1] >= 1000:
                     timed_out = True
                 else:
                     timed_out = False
             else:
                 print(f"Ping to {analyzed[0]} at {ctime()} timed out.")
-                logfile.write(f"""{ctime()} Ping to {analyzed[0]} timed out. """)
+                data = (f"""{ctime()} Ping to {analyzed[0]} timed out.\n""")
+                write_to_log(logfile_path, data)
                 timed_out = True
 
             print_timestamp = time()    # Register a print
             time_outs = add_one(time_outs, timed_out)
 
     if network_change:          # Restart the loop to avoid errors.
-        logfile.write(f"""{ctime()} Network change exception occured. """)
+        data = (f"""{ctime()} Network change exception occured.\n""")
+        write_to_log(logfile_path, data)
         continue
 
     if time_outs == len(address_list):
         print(f"CRITICAL!!!! ALL SERVERS TIMED OUT AT {ctime()}")
-        logfile.write(f"""{ctime()} CRITICAL!!!! ALL SERVERS TIMED OUT """)
-    elif time_outs >= len(address_list) / 2:
+        data = (f"""{ctime()} CRITICAL!!!! ALL SERVERS TIMED OUT\n""")
+        write_to_log(logfile_path, data)
+    elif time_outs >= len(address_list) / 2 and len(address_list) > 2:
         print(f"Attention!! At least half of the servers ({time_outs} servers) timed out at {ctime()}")
-        logfile.write(f"""{ctime()} Attention!! At least half of the servers ({time_outs} servers) timed out """)  
+        data = (f"""{ctime()} Attention!! At least half of the servers ({time_outs} servers) timed out\n""")  
+        write_to_log(logfile_path, data)
 
     time_outs = 0
     timed_out = False
